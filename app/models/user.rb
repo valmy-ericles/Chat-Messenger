36bf0d63f235174eeb8135514f0b92e1f3788d65
Valmy <valmyericles@gmail.com>
2020-04-16 22:03:52 -0300

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+  has_many :contacts
+  has_many :sent, class_name: "Message", foreign_key: "sender_id"
+  has_many :received, class_name: "Message", foreign_key: "receiver_id"
+
+  validates :name, presence: true
+end
